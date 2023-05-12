cd9dcdfb8349b5502e55c2225cf82bc1f1a242ee
Hamza <alfadelhamza@gmail.com>
2023-05-12 21:59:37 +0100

    boiler app

--- backend/app.py
+++ backend/app.py
@@ -6,3 +6,13 @@ app = FastAPI(
     description="LLM API for the Ethglobal Hackathon",
     version="0.0.1",
 )
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the LLM API"}
+
+
+@app.get("/query")
+async def query():
+    return {"message": "Query the LLM API"}
