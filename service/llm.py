9446ca880e9071c01ad081377997fb7444c77252
Hamza <alfadelhamza@gmail.com>
2023-05-14 07:00:47 +0100

    edit llm

--- service/llm.py
+++ service/llm.py
@@ -1,40 +1,28 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.llms import OpenAI
-from langchain.schema import HumanMessage
-from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from langchain import OpenAI, ConversationChain, LLMChain, PromptTemplate
-from langchain.memory import ConversationBufferWindowMemory
-
-from typing import List
-
 from service.model.stream import StreamingChatResponse
-
 from service.prompt_factory import PromptFactory
 from service.constants import OPEN_AI_KEY
+from service.agent import Agent
+from service.utils import get_matic_price
 
 
 class LLM:
     def __init__(self, temperature: float = 0.0, open_api_key: str = OPEN_AI_KEY):
         self.temperature = temperature
         self.open_api_key = open_api_key
-        self.chat = ChatOpenAI(
-            temperature=self.temperature,
-            openai_api_key=self.open_api_key,
-            streaming=True,
-            callbacks=[StreamingStdOutCallbackHandler()],
-            model_name="gpt-4",
-        )
+        self.matic_price_usd = get_matic_price()
+        self.chat = Agent(self.matic_price_usd)
         self.prompt_factory = PromptFactory()
         self.question_count = 0
 
     def main(self, query: str) -> str:
-        if self.question_count < 2:
-            self.question_count += 1
-            prompt = self.prompt_factory.get_prompt("intro", intro=query)
-        elif self.question_count >= 2:
+        if self.question_count == 3:
             prompt = self.prompt_factory.get_prompt(
                 "wallet_connect", wallet_connect=query
             )
+        else:
+            prompt = self.prompt_factory.get_prompt("regular", intro=query)
+
+        self.question_count += 1
 
         stream = StreamingChatResponse(
             chat_openai=self.chat,
@@ -42,4 +30,4 @@ class LLM:
             open_api_key=self.open_api_key,
         )
 
-        return stream(prompt).content
+        return stream(prompt)
